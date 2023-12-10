56d3ee0d15ed6fd4a6dd8c3439762ab409cfe538
felipeCies <cieslick246@gmail.com>
2023-12-10 06:31:42 -0300

    attt3

--- src/main/java/com/senac/Estoque/controller/ProdutoController.java
+++ src/main/java/com/senac/Estoque/controller/ProdutoController.java
@@ -3,9 +3,11 @@ package com.senac.Estoque.controller;
 import com.senac.Estoque.model.Produto;
 import com.senac.Estoque.repository.ProdutoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 @Controller
 @RequestMapping("/produtos")
@@ -45,6 +47,7 @@ public class ProdutoController {
         produtoRepository.save(produto);
         return "redirect:/produtos";
     }
+
 
 
 
